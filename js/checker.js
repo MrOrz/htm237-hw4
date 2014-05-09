a901578fb90588676003ac3467b62cddd3bf28fb
MrOrz <johnsonliang7@gmail.com>
2014-05-09 20:39:49 +0800

    Practice 3: Get user groups

--- js/checker.js
+++ js/checker.js
@@ -32,11 +32,15 @@ window.fbAsyncInit = function(){
 
     // 1. 讓使用者登入此 Facebook App (FB.login)
     FB.login(function(){
-      // 2. 以 FB.api 拿到使用者的 group 列表
-      // 拿到使用者 group 列表的 response 之後：
-      // results.after('<div class="hw4-complete alert alert-info">掃描完成</div>');
       console.log('Logged in!');
 
+      // 2. 以 FB.api 拿到使用者的 group 列表
+      FB.api('/me/groups', function(resp){
+        console.log('User groups:', resp.data);
+        // 拿到使用者 group 列表的 response 之後：
+        // results.after('<div class="hw4-complete alert alert-info">掃描完成</div>');
+
+      });
     },{scope: 'user_groups'});
   });
 };
